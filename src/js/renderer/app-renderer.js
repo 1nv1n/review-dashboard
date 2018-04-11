92904da9cef831c331a6ee499b0a3adf831cf32f
Pathare <NP032041@cerner.net>
2018-04-11 17:59:52 -0500

    Hiding AppWrapper on Modal Open

--- src/js/renderer/app-renderer.js
+++ src/js/renderer/app-renderer.js
@@ -1,8 +1,23 @@
 /**
+ * Main JS for the "Renderer".
+ * Look at IPC JS for operations stemming from events sent up from the server.
+ */
+
+/**
  * Reload the current page (force-refresh)
  */
 function reloadPage() {
   window.location.reload(true);
+}
+
+/**
+ * Backout
+ */
+function blackout() {
+  var appWrapper = document.getElementById("appWrapper");
+  if(!appWrapper.classList.contains("blackout")) {
+    appWrapper.classList.add("blackout");
+  }
 }
 
 /**
